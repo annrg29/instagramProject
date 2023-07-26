0aca39778a492d6508c2ead958c4b1d29d48e5c5
Anurag Kumar Thakur <69040347+ann29-tdk@users.noreply.github.com>
2023-07-26 20:24:22 +0530

    Add files via upload

---
+++ client/build/precache-manifest.887294a70c81d4c76cde6c24e159aef0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b44e8547eefe2053aaf87eb646d3b24d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbcd0d882a3212b30444",
+    "url": "/static/css/main.a4135bf3.chunk.css"
+  },
+  {
+    "revision": "70c9184443c804ee3210",
+    "url": "/static/js/2.05df021f.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/static/js/2.05df021f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbcd0d882a3212b30444",
+    "url": "/static/js/main.555cb867.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  }
+]);
