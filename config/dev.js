a155611e34ee24c60884bf08bbe1c3aeae2581b0
Anurag Kumar Thakur <69040347+ann29-tdk@users.noreply.github.com>
2023-07-26 20:22:56 +0530

    added dev.js

---
+++ config/dev.js
@@ -0,0 +1,10 @@
+module.exports = {
+  MONGOURL:
+    "mongodb+srv://test-anurag:test123@cluster0.g2tqnwo.mongodb.net/instagramClone",
+  JWT_SECRET: "shobhitjain",
+  SENDGRID_API:
+    "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",
+  EMAIL: "http:localhost:3000",
+
+  //"mongodb+srv://User:test123@cluster0.dho7rvy.mongodb.net/instagram",
+};
